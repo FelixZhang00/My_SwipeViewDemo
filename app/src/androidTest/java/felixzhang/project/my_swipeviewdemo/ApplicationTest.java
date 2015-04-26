a298d80cf189fc50fd196ac737427e6d88903e19
FelixZhang00 <l836828946@gmail.com>
2015-04-26 11:34:04 +0800

    complete single swipeview.

---
+++ app/src/androidTest/java/felixzhang/project/my_swipeviewdemo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package felixzhang.project.my_swipeviewdemo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
